ce97069369a5ece41709dd6ecb52558cf315f7b3
Mmalga <82066326+Mmalga@users.noreply.github.com>
2021-06-29 19:32:11 -0300

    ahora si en el repo correcto

--- src/parcial.hs
+++ src/parcial.hs
@@ -1,2 +1,39 @@
 module Library where
 import PdePreludat
+
+--1
+
+data Torta = Torta {
+    nombre:: String,
+    harina:: Number,
+    huevos:: Number,
+    calorias:: Number,
+    ingredientes:: [Ingredientes]
+} deriving Show
+
+
+type Ingredientes = Torta -> Torta
+
+
+
+--2
+aumentarCalorias::Number -> Ingredientes
+aumentarCalorias calorias torta = torta  {calorias = (calorias $ torta) + calorias}
+
+--a) 
+chocolate::Number -> Ingredientes
+chocolate gramos torta = aumentarCalorias ((*gramos).huevos $ torta)  torta 
+
+
+--b)
+frutal::String -> Number ->Ingredientes
+frutal fruta gramos torta = aumentarCalorias ((/2).(*gramos).(length) $ fruta) $ torta 
+
+
+--c) 
+puñadoDeFrutas::Ingredientes
+puñadoDeFrutas torta =  torta  {calorias = calorias torta +  100}
+
+--d)
+bañoDeCrema::Ingredientes
+bañoDeCrema torta = torta  {calorias = calorias torta +  50 + harina torta}
